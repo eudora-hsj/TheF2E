d7566f82aacef8d6238c19fd2fe3a0d2e753e620
eudora-hsj <eudora.hsj@gmail.com>
2021-04-01 16:16:45 +0800

    add fontawesome

--- src/main.js
+++ src/main.js
@@ -6,11 +6,19 @@ import router from './router'
 import 'reset-css'
 import 'animate.css'
 import Element from 'element-ui'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+// import { faPlus } from '@fortawesome/free-solid-svg-icons'
+// library.add(faPlus)
 // import {
 //   Select,
 //   Button
 //   // ...
-// } from 'element-ui'
+// } from ' element-ui'
+library.add(fas, far)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.use(Element)
 //  Vue.component(Select.name, Select)
