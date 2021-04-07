f1d0e8c2eced363969afff5fa474f6da27fbfe64
eudora-hsj <eudora.hsj@gmail.com>
2021-04-07 17:42:10 +0800

    import google font

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@ export default {
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Roboto+Condensed');
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
 }
