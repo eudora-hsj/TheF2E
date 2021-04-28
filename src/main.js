19e3994b8e30a7ba132e929ff35f89065646aeb7
eudora-hsj <eudora.hsj@gmail.com>
2021-04-28 11:38:01 +0800

    add vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
 import router from './router'
 import 'reset-css'
@@ -27,6 +28,7 @@ Vue.prototype.HOST = '/api'
 library.add(fas, far)
 Vue.component('fas-icon', FontAwesomeIcon)
 
+Vue.use(Vuex)
 Vue.use(ElementUI)
 //  Vue.component(Select.name, Select)
 
